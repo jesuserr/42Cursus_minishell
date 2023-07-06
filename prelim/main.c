2bd49cb3bcd14f0b9dd162ccdfa6a1da625ee748
Jesuserr <ivoice666@hotmail.com>
2023-07-06 21:52:54 +0200

    Repository cleaning

--- prelim/main.c
+++ prelim/main.c
@@ -6,7 +6,7 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 10:58:10 by jesuserr          #+#    #+#             */
-/*   Updated: 2023/07/02 11:49:43 by jesuserr         ###   ########.fr       */
+/*   Updated: 2023/07/06 21:39:46 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	execute(char *cmd, char **envp)
 		ft_error_handler(ERROR_XXX, NULL);
 }
 
-void	child_process(int fd[2], char **argv, char **envp)
+void	child_process_1(int fd[2], char **argv, char **envp)
 {
 	int	file;
 
@@ -47,7 +47,7 @@ void	child_process(int fd[2], char **argv, char **envp)
 	execute(argv[2], envp);
 }
 
-void	parent_process(int fd[2], char **argv, char **envp)
+void	child_process_2(int fd[2], char **argv, char **envp)
 {
 	int	file;
 
@@ -67,20 +67,26 @@ void	parent_process(int fd[2], char **argv, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
-	pid_t	pid;
+	pid_t	pid[2];
 
-	printf("%s\n", readline("test> "));
 	if (argc == 5)
 	{
 		if (pipe(fd) == -1)
 			ft_error_handler(ERROR_XXX, NULL);
-		pid = fork();
-		if (pid == -1)
+		pid[0] = fork();
+		if (pid[0] == -1)
 			ft_error_handler(ERROR_XXX, NULL);
-		if (pid == 0)
-			child_process(fd, argv, envp);
-		parent_process(fd, argv, envp);
-		waitpid(pid, NULL, 0);
+		if (pid[0] == 0)
+			child_process_1(fd, argv, envp);
+		pid[1] = fork();
+		if (pid[1] == -1)
+			ft_error_handler(ERROR_XXX, NULL);
+		if (pid[1] == 0)
+			child_process_2(fd, argv, envp);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pid[0], NULL, 0);
+		waitpid(pid[1], NULL, 0);
 	}
 	else
 		ft_error_handler(ERROR_ARGS, NULL);
