9819e5343c0a33b384fc4a9d8bb545f8e83180db
cescanue <cescanue@student.42barcelona.com>
2023-08-01 16:04:01 +0200

    signals crash

--- srcs/minishell_heredoc.c
+++ srcs/minishell_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: cescanue <cescanue@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 10:50:30 by cescanue          #+#    #+#             */
-/*   Updated: 2023/08/01 15:23:07 by cescanue         ###   ########.fr       */
+/*   Updated: 2023/08/01 16:03:30 by cescanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_heredoc(char *sep)
 	str = 0;
 	pipe(p);
 	ft_printf(1, "> ");
-	//str = get_next_line(0);
+	str = get_next_line(0);
 	while (str && (!ft_strnstr(str, sep, ft_strlen(str))
 			|| (ft_strlen(str) - 1) != ft_strlen(sep)))
 	{
