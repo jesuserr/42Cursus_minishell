8e8c9f6a5ba9970665aac5d860a7c407f013cca9
cescanue <cescanuela@gmail.com>
2023-09-08 22:10:33 +0200

    bug echo "\'cescanue\'" fixed

--- srcs/minishell_parser_to_struct_utils.c
+++ srcs/minishell_parser_to_struct_utils.c
@@ -6,7 +6,7 @@
 /*   By: cescanue <cescanue@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:01:25 by cescanue          #+#    #+#             */
-/*   Updated: 2023/09/04 21:53:55 by cescanue         ###   ########.fr       */
+/*   Updated: 2023/09/08 21:59:03 by cescanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,23 +38,31 @@ int	p_t_s_count(char *block)
 	return (count);
 }
 
+void	p_strnstr1(const char *h, const char *n, size_t *c, size_t *c1)
+{
+	if (n[*c1] && h[*c + *c1] == n[*c1]
+		&& *c + *c1 && (h[*c + *c1 + 1] == ' '
+			|| h[*c + *c1 + 1] == 0))
+		(*c)++;
+}
+
 char	*p_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	count;
 	size_t	count1;
+	int		dq;
 
 	count = 0;
+	dq = 0;
 	while (haystack[count] && count < len)
 	{
-		if (haystack[count] == '\'')
+		if (haystack[count] == '\"')
+		dq++;
+		if (haystack[count] == '\'' && !(dq % 2))
 			while (haystack[++count] && haystack[++count] != '\'')
 				;
 		count1 = 0;
-		if (needle[count1] && haystack[count + count1] == needle[count1]
-			&& count + count1 < len && (haystack[count + count1 + 1] == ' '
-				|| haystack[count + count1 + 1] == 0
-				|| haystack[count + count1 + 1] == '"'))
-			count++;
+		p_strnstr1(haystack, needle, &count, &count1);
 		while (needle[count1] && haystack[count + count1] == needle[count1]
 			&& count + count1 < len)
 			count1++;
